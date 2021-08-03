c8752f8f016c656f043d7e9c730816ca68918ace
carolareas <86005932+carolareas@users.noreply.github.com>
2021-08-03 19:11:56 -0300

    Update aluno_ex9.hpp

--- aluno_ex9.hpp
+++ aluno_ex9.hpp
@@ -16,5 +16,34 @@ enum EnumEx9
 EnumEx9
 exercicio9(int v[], int tamanho)
 {
+  int cont1, cont2, razao1, razao2, i;
+   cont1=0;
+   cont2=0;
+   razao1= v[1]-v[0];
+   razao2= v[1]/v[0];
+
+//PA
+for(i=0; i<tamanho; i++){
+    if (v[i+1]-v[i]==razao1){
+        cont1+=1;
+}
+
+}
+//PG
+for(i=0; i<tamanho; i++){
+
+    if (v[i+1]/v[i]==razao2){
+        cont2+=1;
+
+}
+}
+if (cont1==tamanho-1){
+    return PA;
+}
+
+if (cont2==tamanho-1){
+    return PG;
+}
+
   return Outro; // PA ou PG
 }
